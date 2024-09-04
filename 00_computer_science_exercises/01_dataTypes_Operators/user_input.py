415fc7a2bae718b57df0ec4575f44a07c552ac2d
Datboiketh <s10780674@students.duvalschools.org>
2024-09-04 13:20:47 -0400

    float, str, and int

--- 00_computer_science_exercises/01_dataTypes_Operators/user_input.py
+++ 00_computer_science_exercises/01_dataTypes_Operators/user_input.py
@@ -20,6 +20,19 @@ print(myNumber + 5)
 
 # Wrapper Functions
 # int() will covert the data to an interger if possiable.
+newNumber = input("Please type a value and press enter. \n")
+print(int(newNumber))
+print(float(newNumber))
+print(str(newNumber))
+
 # float() will convert the data to a float if possible.
+newNumber = input("Please type a value and press enter. \n")
+#print(int(newNumber)) <-- cannot FLOAT to INTERGER.
+print(float(newNumber)) # can convert STRRING to float
+print(str(newNumber)) # can convert float to string.
+
 # str() will convert  the data to a String if possible.
-er
+newNumber = input("Please type a value and press enter. \n")
+print(float(newNumber))
+print(str(newNumber))
+
