395a762f8c2e16870f353b377d252e78852b9ca0
Datboiketh <s10780674@students.duvalschools.org>
2024-09-10 13:22:00 -0400

    award extra lives

---
+++ 01_gaming_exercises/00_awarding_extra_lives/awarding_extra_lives.py
@@ -0,0 +1,16 @@
+# Award extra lives, Keith Dinkins, V0.0
+
+lives = 3
+score = 1000
+
+# allow the user to input the score as an integer
+
+# allow user to input score
+if score < 10000:
+    print("lose a life")
+elif score > 10000 <= 100001:
+    print("Give 1 extra life")
+if score > 100000:
+    print("Give 2 extra lives")
+
+# Output the score and number of lives to the screen.
