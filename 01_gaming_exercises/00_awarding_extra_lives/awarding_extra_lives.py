df9223f1492c567baf4efdbff86878017e1ce230
Datboiketh <s10780674@students.duvalschools.org>
2024-09-12 13:21:10 -0400

    updated lives

--- 01_gaming_exercises/00_awarding_extra_lives/awarding_extra_lives.py
+++ 01_gaming_exercises/00_awarding_extra_lives/awarding_extra_lives.py
@@ -1,8 +1,8 @@
 # Award extra lives, Keith Dinkins, V0.0
 
 lives = 3
-score = 100000
-name = "Keth"
+score = 100
+name = "Keith"
 print(f"Hello{name}! You scored {score} points.\n")
 
 # allow the user to input the score as an integer
@@ -10,9 +10,14 @@ print(f"Hello{name}! You scored {score} points.\n")
 # allow user to input score
 if score <= 10000:
     print("lose a life")
+    lives -= 1
 elif score < 100001:
     print("Give 1 extra life")
+    lives += 1
 elif score > 100000: 
     print("Give 2 extra lives")
+    lives += 2 
 
+print("live: " + str(lives))
+print("score: " + str(score))
 # Output the score and number of lives to the screen.
