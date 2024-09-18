c94dccf1c2ada533c98f436af359fadbb6732d89
Datboiketh <s10780674@students.duvalschools.org>
2024-09-18 12:04:34 -0400

    v0.2 is correct

--- 01_gaming_exercises/04_rock_paper_scissors/Rock_paper_scissors.py
+++ 01_gaming_exercises/04_rock_paper_scissors/Rock_paper_scissors.py
@@ -1,4 +1,4 @@
-# Rock , Paper, Scissors Keith D. v0.1
+# Rock , Paper, Scissors Keith D. v0.2
 
 # MODULE IMPORTS
 import random
@@ -11,3 +11,12 @@ playerChoice = None
 # DATA STRUCURES
 cpuScore = 0
 cpuChoice = None
+
+#PLAYER NAME INPUT
+playerName = input("Type name and press enter.\n")
+print(f"Hello {playerName}!")
+isCorrect = input("Is this correct? Type yes or no and press enter. \n").lower()
+if isCorrect == "yes": 
+    print(f"ok {playerName}, let's play, Rock, Paper, Scissors!")
+else:
+    playerName = input("Type your name and press enter.\n")
