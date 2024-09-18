23c435ce7d24f69afc39bee67eeb9d8a7f0f0b5a
Datboiketh <s10780674@students.duvalschools.org>
2024-09-18 11:50:25 -0400

    v0.1 new varibles

---
+++ 01_gaming_exercises/04_rock_paper_scissors/Rock_paper_scissors.py
@@ -0,0 +1,13 @@
+# Rock , Paper, Scissors Keith D. v0.1
+
+# MODULE IMPORTS
+import random
+
+# DATA STRUCURES -- Player
+playerName = "Test Player"
+playerScore = 0
+playerChoice = None
+
+# DATA STRUCURES
+cpuScore = 0
+cpuChoice = None
