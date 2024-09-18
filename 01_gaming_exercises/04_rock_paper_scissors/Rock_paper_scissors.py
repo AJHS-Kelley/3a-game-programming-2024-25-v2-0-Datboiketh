d8c2b418c8a045a4dd777ea872649c091085fb3b
Datboiketh <s10780674@students.duvalschools.org>
2024-09-18 12:54:46 -0400

    v0.3 Multi-Line strings

--- 01_gaming_exercises/04_rock_paper_scissors/Rock_paper_scissors.py
+++ 01_gaming_exercises/04_rock_paper_scissors/Rock_paper_scissors.py
@@ -1,4 +1,4 @@
-# Rock , Paper, Scissors Keith D. v0.2
+# Rock , Paper, Scissors Keith D. v0.3
 
 # MODULE IMPORTS
 import random
@@ -16,7 +16,27 @@ cpuChoice = None
 playerName = input("Type name and press enter.\n")
 print(f"Hello {playerName}!")
 isCorrect = input("Is this correct? Type yes or no and press enter. \n").lower()
+
+# .lower() turns all into lower case
+# .upper() turns all into upper case
+
 if isCorrect == "yes": 
-    print(f"ok {playerName}, let's play, Rock, Paper, Scissors!")
+    print(f"Ok {playerName}, let's play, Rock, Paper, Scissors!")
 else:
     playerName = input("Type your name and press enter.\n")
+
+# THE RULES
+print(f"""
+Welcome {playerName} to the Rock, Paper, Scissors Robot!
+It's time to play  Rock, Paper, Scissors!
+
+You will play agasint an AI. the first one to score 5 points wins.
+You will select from Rock, Paper or scissors.
+The CPU will select Rock, Paper, or Scissors.
+
+1) Rock beats Scissors
+2) Scissors beats Paper
+3)Paper beats Rock
+""")
+
+# MULTI-LINE STRINGS CAN BE USED AS COMMENTS
