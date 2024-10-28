1a82519d8d498a4b33ee4fcae161d0eb6d028364
Datboiketh <s10780674@students.duvalschools.org>
2024-10-28 12:59:34 -0400

    def argument 1

---
+++ 00_computer_science_exercises/05_functions/functionsPractices.py
@@ -0,0 +1,41 @@
+# Functions practice, Keith DInkins, v0.1
+
+import random
+
+def helloWorldMulti(): # FUNCTION SIGNATURE
+    """This Funtion will output Hello, world! in a language the user choose. """ # DoCSTRING 
+    # print a list of languages to the screen, at least three but no more than five.
+    # allow the use to selct input a choice for the languauge.
+    print("""Hello User you will have the option to pick three languages from what I have avalaible
+          These are the languages the are available
+          [S]panish
+          [J]apanese
+          [F]rench
+          [C]hinese
+          [E]nglish
+            """)
+    
+    language = input("Please put the first letter of What language would you like to pick?\n").lower()
+
+    if language == "s":
+        print("Hola Mund")
+    elif language == "j":
+        print("Kon'nichiwa sekai")
+    elif language == "f":
+        print("Bonjour le monde")
+    elif language == "c":
+        print("Nǐ hǎo shìjiè")
+    elif language == "e":
+        print("Hello world")    
+    else:
+        print("unable to find langauge")
+        exit()
+
+helloWorldMulti()
+
+
+argument1 = random.randint(-1000, 1000)
+    
+def isEven(argument1: int) -> bool: 
+    """Determines if interger value is even or odd."""
+if argument1 % 2 == ?: 
